4999bfc9463f3206bbcbab421ef42f24a0617260
Armellys <adele.journee@hotmail.fr>
2017-04-07 19:53:13 +0200

    fin de la partie 1 du TD Maven

--- aj/src/main/java/aj/App.java
+++ aj/src/main/java/aj/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String nom = "adele";
+        System.out.println( "Hello World! "+nom );
+        System.out.println( "Resultat max entre 4 et 5 :  "+ max(4,5) );
+    }
+    public static int max (int a,int b){
+    	//return a>b ? a:b;
+    	return a;
     }
 }
